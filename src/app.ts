90cee433b95acd1d5250cec39b00f9ca74fa497a
faiz8411 <faizullah.noa2014@gmail.com>
2023-08-04 17:05:11 +0600

    lint staged setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-const app: Application = express();
-const port = 5000;
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.get("/", (req: Request, res: Response) => {
-  res.send("work done");
-});
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.get('/', (req: Request, res: Response) => {
+  res.send('work done')
+})
 
-export default app;
+export default app
